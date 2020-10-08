d922b572cf628f9cf97b9655ffe3f55bc5d2141a
moxiaomomo <moxiaomomo@gmail.com>
2020-10-08 23:51:13 +0800

    Update logic to get location.

--- src/pages/history/index.vue
+++ src/pages/history/index.vue
@@ -1,22 +1,44 @@
 <template>
-  <div id="hisMainContent" class="home-container"></div>
+  <div
+    id="hisMainContent"
+    class="home-container"
+  ></div>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
 
-import { Component, Vue, Prop } from 'vue-property-decorator'
+import { Component, Vue, Prop } from "vue-property-decorator";
+import ApiService from "@/api/api.service";
 
 @Component({
   components: {},
 })
 export default class HistoryPage extends Vue {
-  private sidPrefix = 'lbsclient_'
-  private asOpen: boolean = true
+  private sidPrefix = "lbsclient_";
+  private asOpen: boolean = true;
 
   public mounted(): void {
-    const content = document.getElementById('hisMainContent') as HTMLElement
-    content.style.height = `${window.innerHeight - 100}px`
+    const content = document.getElementById("hisMainContent") as HTMLElement;
+    content.style.height = `${window.innerHeight - 100}px`;
+
+    this.initHistory();
+  }
+
+  async initHistory() {
+    const user = uni.getStorageSync("user");
+    const userRole = uni.getStorageSync("userRole");
+    const data = {
+      userId: user.userId,
+      role: userRole,
+    };
+    const res: any = await ApiService.post(
+      `/passenger/order/queryOrderHis`,
+      data
+    );
+    if (res.data) {
+      console.log(res.data);
+    }
   }
 }
 </script>
