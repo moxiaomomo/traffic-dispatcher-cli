cdde152d046a9bbb495c210ed9aa564091f376f2
moxiaomomo <moxiaomomo@gmail.com>
2020-10-13 01:33:44 +0800

    Update logic to get location.

--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,6 +1,22 @@
 <template>
-  <div id="myMainContent" class="home-container">
-    <button class="mini-btn logout" type="warn" @click="onLogout">
+  <div
+    id="myMainContent"
+    class="home-container"
+  >
+    <uni-card
+      :title="userName"
+      mode="style"
+      :is-shadow="true"
+      thumbnail=http://ttcli.ibitcoding.com/image/my1.jpg"
+      extra="上次登录 Guangzhou 2020-10-10 12:32:19"
+    >
+      出行里数: 3000公里<br>会员积分: 320
+    </uni-card>
+    <button
+      class="mini-btn logout"
+      type="warn"
+      @click="onLogout"
+    >
       注销登录
     </button>
   </div>
@@ -9,32 +25,36 @@
 <script lang="ts">
 // @ is an alias to /src
 
-import { Component, Vue, Prop } from 'vue-property-decorator'
+import { Component, Vue, Prop } from "vue-property-decorator";
 
 @Component({
   components: {},
 })
 export default class MyPage extends Vue {
-  private sidPrefix = 'lbsclient_'
-  private asOpen: boolean = true
+  private sidPrefix = "lbsclient_";
+  private asOpen: boolean = true;
+  private userName: string = "";
 
   public mounted(): void {
-    const content = document.getElementById('myMainContent') as HTMLElement
-    content.style.height = `${window.innerHeight - 100}px`
+    const content = document.getElementById("myMainContent") as HTMLElement;
+    content.style.height = `${window.innerHeight - 100}px`;
+
+    const user = uni.getStorageSync("user");
+    this.userName = user.userName;
   }
 
   onLogout(evt: any) {
     try {
       // 删除user缓存
-      uni.removeStorageSync('user')
+      uni.removeStorageSync("user");
       // 跳转到登录页
-      uni.reLaunch({ url: '/pages/signin/index' })
+      uni.reLaunch({ url: "/pages/signin/index" });
     } catch (e) {
       // error
       uni.showToast({
-        title: '注销异常:' + e,
-        icon: 'none',
-      })
+        title: "注销异常:" + e,
+        icon: "none",
+      });
     }
   }
 }
